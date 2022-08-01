e34b58b2f5840a059e4cc1e32b6fe2669e3f7578
themesanasang <chakrit.cpe@gmail.com>
2022-08-01 15:41:08 +0700

    edit payable type model

--- server/models/payable_type.js
+++ server/models/payable_type.js
@@ -26,7 +26,7 @@ module.exports = knex => {
     .timeout(timeout)
 
     const findAll = () => knex.select(
-        'account_id', 'payable_type_name', 'status'
+        'payable_type_id', 'account_id', 'payable_type_name', 'status'
         , knex.raw('(SELECT account FROM ap_account WHERE ap_account.account_id=ap_payable_type.account_id) AS account')
     )
     .from(tableName)
@@ -34,7 +34,7 @@ module.exports = knex => {
     .timeout(timeout)
 
     const findOne = (payable_type_id) => knex.select(
-        'account_id', 'payable_type_name', 'status'
+        'payable_type_id', 'account_id', 'payable_type_name', 'status'
         , knex.raw('(SELECT account FROM ap_account WHERE ap_account.account_id=ap_payable_type.account_id) AS account')
     )
     .from(tableName)
